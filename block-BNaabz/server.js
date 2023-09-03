d9049f40009f38c0ac15a73a47ea0be89bc645f5
Anand Seshadri <78548214+anandsesha@users.noreply.github.com>
2023-09-03 17:19:31 -0400

    refactor

--- block-BNaabz/server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/india');
-
-app.listen(3000, () => {
-  console.log(`Server is listening on port 3000`);
-});
